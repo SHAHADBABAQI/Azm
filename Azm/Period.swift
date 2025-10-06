2e655f74ae3cab6726ddaa80c48c456a17f1d497
jumana <j9oo@icloud.com>
2025-10-06 11:29:24 +0300

    navi

--- Azm/Period.swift
+++ Azm/Period.swift
@@ -9,91 +9,99 @@ import SwiftUI
 
 struct ContentView2: View {
     var body: some View {
-        ZStack {
-            //النص حق اللغة
-            Image("Image 13")
-                .ignoresSafeArea()
-            Text("اختر المدة")
-                .font(.system(size: 25, weight: .bold, design: .default))
+        NavigationStack {
+            ZStack {
+                //النص حق اللغة
+                Image("Image 13")
+                    .ignoresSafeArea()
+                Text("اختر المدة")
+                    .font(.system(size: 25, weight: .bold, design: .default))
+                    .foregroundColor(.black)
+                    .bold()
+                    .font(.title)
+                    .padding(.top, -300)
+                    .font(.custom("SF Arabic Round", size: 36))
+                
+                
+                //الزر الاول حق الشهر
+                Button("٣٠ يوم") {
+                    print("٣٠ يوم")
+                }
+                .padding()
+                .frame(width: 263)
+                .background(Color(.systemGray6))
+                .cornerRadius(12)
                 .foregroundColor(.black)
+                .padding(.top, -150)
                 .bold()
-                .font(.title)
-                .padding(.top, -300)
-                .font(.custom("SF Arabic Round", size: 36))
-
-            
-            //الزر الاول حق الشهر
-            Button("٣٠ يوم") {
-                print("٣٠ يوم")
+                
+                // الزر الثاني
+                Button("٦٠ يوم") {
+                    print("٦٠ يوم")
+                }
+                .padding()
+                .frame(width: 263)
+                .background(Color(.systemGray6))
+                .cornerRadius(12)
+                .foregroundColor(.black)
+                .padding(.top, -60)
+                .bold()
+                
+                //الزر الثالث
+                Button("٩٠ يوم") {
+                    print("٦٠ يوم")
+                }
+                .padding()
+                .frame(width: 263)
+                .background(Color(.systemGray6))
+                .cornerRadius(12)
+                .foregroundColor(.black)
+                .padding(.top, 110)
+                .bold()
+                
+             
+                    // السهم الاول
+                NavigationLink {
+                    azm ()
+                    }
+                     label: {
+               
+                        Image(systemName: "arrow.backward") }
+                    .padding()
+                    .background(Color(.white))
+                    .foregroundStyle(.black)
+                    .clipShape(Capsule())
+                    .font(.system(size: 20))
+                    .foregroundColor(.white)
+                    .offset(y: 350)
+                    .offset(x: -150)
+                    
+                    //السهم الثاني
+                NavigationLink {
+                    habitview()
+                    }
+                     label: {
+                        Image(systemName: "arrow.forward") }
+                    .padding()
+                    .background(Color(.white))
+                    .foregroundStyle(.black)
+                    .clipShape(Capsule())
+                    .font(.system(size: 20))
+                    .foregroundColor(.white)
+                    .offset(y: 350)
+                    .offset(x: 150)
+                    
+                    
+                    //صورة الجمل
+                    Image("Image 2 m")
+                        .padding(.leading, 307)
+                        .padding(.top,420)
+                }
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
             }
-            .padding()
-            .frame(width: 263)
-            .background(Color(.systemGray6))
-            .cornerRadius(12)
-            .foregroundColor(.black)
-            .padding(.top, -150)
-            .bold()
-            
-            // الزر الثاني
-            Button("٦٠ يوم") {
-                print("٦٠ يوم")
-            }
-            .padding()
-            .frame(width: 263)
-            .background(Color(.systemGray6))
-            .cornerRadius(12)
-            .foregroundColor(.black)
-            .padding(.top, -60)
-            .bold()
-            
-            //الزر الثالث
-            Button("٩٠ يوم") {
-                print("٦٠ يوم")
-            }
-            .padding()
-            .frame(width: 263)
-            .background(Color(.systemGray6))
-            .cornerRadius(12)
-            .foregroundColor(.black)
-            .padding(.top, 110)
-            .bold()
+        }
     
-            
-            // السهم الاول
-            Button{ } label: {
-                    Image(systemName: "arrow.backward") }
-            .padding()
-            .background(Color(.white))
-            .foregroundStyle(.black)
-            .clipShape(Capsule())
-            .font(.system(size: 20))
-            .foregroundColor(.white)
-            .offset(y: 350)
-            .offset(x: -150)
-            
-            //السهم الثاني
-            Button{ } label: {
-                Image(systemName: "arrow.forward") }
-            .padding()
-            .background(Color(.white))
-            .foregroundStyle(.black)
-            .clipShape(Capsule())
-            .font(.system(size: 20))
-            .foregroundColor(.white)
-            .offset(y: 350)
-            .offset(x: 150)
-            
-            
-            //صورة الجمل
-            Image("Image 2 m")
-                .padding(.leading, 307)
-                .padding(.top,420)
-            }
-                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-                }
-                }
-
-
+}
 #Preview {
     ContentView2()
 }
